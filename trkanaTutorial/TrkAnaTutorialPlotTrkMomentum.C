d397604b9b06a0f5de427f6d7b651829d1cd2c1d
Andrew Boldy <aboldy@mu2egpvm01.fnal.gov>
2025-01-24 11:39:00 -0600

    Updates again

--- trkanaTutorial/TrkAnaTutorialPlotTrkMomentum.C
+++ trkanaTutorial/TrkAnaTutorialPlotTrkMomentum.C
@@ -22,19 +22,23 @@ void TrkAnaTutorialPlotTrkMomentum()
     input_filelist.close();
   }
 
+  cout << "Number of entries: " << trkana->GetEntries() << endl;
+
   TCanvas* c1 = new TCanvas("c1","c1");
   c1->SetGridx(true);
   c1->SetGridy(true);
 
-  TH1D* hist = new TH1D("hist","",100,0,110);
+  TH1D* hist = new TH1D("hist","",100,100,110);
   trkana->Draw("demfit.mom.R()>>hist","","HIST"); //utilizing the R() functions. Unsure wha this is, but will come back with more information; plot the magnitude of the momentum vector
 
-  TH1D* hist2 = new TH1D("hist2","",100,0,110);
+  TH1D* hist2 = new TH1D("hist2","",100,100,110);
   trkana->Draw("demfit.mom.R()>>hist2", "demfit.sid==0", "HIST SAME"); //again utilizing R() function. now tracking only at one intersection by establishing a cut using the center argument; sid = surface id, sid=0 denotes the entrance to the tracker
 
-  TH1D* hist3= new TH1D("hist3","",100,0,110);
+  TH1D* hist3= new TH1D("hist3","",100,100,110);
   trkana->Draw("demfit.mom.R()>>hist3","demfit.sid==0 && demlh.t0>=700", "HIST SAME"); //utilizing R() to make cuts at the entrance of the tracker and also at the time greater than 700 (ms?)
 
+  c1->Update();
+  cout << "Saving canvas as trkMomentum.pdf" << endl;
   c1->SaveAs("trkMomentum.pdf");
 
   delete c1;
