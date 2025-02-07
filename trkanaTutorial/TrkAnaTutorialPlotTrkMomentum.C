5b4651f2065ae75d6ae92a9f46d783bae5c1ce9a
Andrew Boldy <aboldy@mu2egpvm02.fnal.gov>
2025-02-07 14:30:34 -0600

    Updating the Tutorial Plots to Github

--- trkanaTutorial/TrkAnaTutorialPlotTrkMomentum.C
+++ trkanaTutorial/TrkAnaTutorialPlotTrkMomentum.C
@@ -1,3 +1,6 @@
+//TrkAnaTutorial Plotting Detected Conversion Electron Momenta with Varying cuts
+//Written by Andrew Boldy University of South Carolina
+//Spring 2025
 
 //all of my inclusions
 #include <TChain.h>
@@ -9,10 +12,7 @@
 #include <fstream>
 #include <iostream>
 
-bool DO_ELECTRON_MOMENTUM = false;
-bool DO_ELECTRON_ORIGIN_MOMENTUM_COMP = true;
-bool DO_POSITRON_MOMENTUM = false;
-bool DO_POSITRON_ORIGIN_MOMENTUM = false;
+bool DO_ELECTRON_MOMENTUM = true;
 
 using namespace std;
 
@@ -40,6 +40,7 @@ void TrkAnaTutorialPlotTrkMomentum()
 
     TH1D* hist = new TH1D("hist","",100,100,110);
     trkana->Draw("demfit.mom.R()>>hist","","HIST"); //utilizing the R() functions. Unsure wha this is, but will come back with more information; plot the magnitude of the momentum vector
+    hist->SetLineColor(kBlue);
 
     TH1D* hist2 = new TH1D("hist2","",100,100,110);
     trkana->Draw("demfit.mom.R()>>hist2", "demfit.sid==0", "HIST SAME"); //again utilizing R() function. now tracking only at one intersection by establishing a cut using the center argument; sid = surface id, sid=0 denotes the entrance to the tracker
@@ -63,99 +64,5 @@ void TrkAnaTutorialPlotTrkMomentum()
     delete hist;
     delete hist2;
     delete hist3;
-  }
-  if (DO_POSITRON_MOMENTUM==true)
-  {
-    TChain* trkana = new TChain("TrkAna/trkana");
-
-    ifstream input_filelist("filelists/nts.mu2e.CePlusEndpointMix1BB.MDC2020s_perfect_v1_0_std_v01_01_00.list");
-    if (input_filelist.is_open())
-    {
-      string filename;
-      while(getline(input_filelist,filename))
-      {
-        trkana->Add(filename.c_str());
-      }
-      input_filelist.close();
-    }
-
-    cout << "Number of entries: " << trkana->GetEntries() << endl;
-    TCanvas* c1 = new TCanvas("c1","c1");
-    c1->SetGridx(true);
-    c1->SetGridy(true);
-
-    TH1D* hist = new TH1D("hist","",100,100,110);
-    trkana->Draw("demfit.mom.R()>>hist","","HIST"); //utilizing the R() functions. Unsure wha this is, but will come back with more information; plot the magnitude of the momentum vector
-
-    TH1D* hist2 = new TH1D("hist2","",100,100,110);
-    trkana->Draw("demfit.mom.R()>>hist2", "demfit.sid==0", "HIST SAME"); //again utilizing R() function. now tracking only at one intersection by establishing a cut using the center argument; sid = surface id, sid=0 denotes the entrance to the tracker
-    hist2->SetLineColor(kRed);
-
-    TH1D* hist3= new TH1D("hist3","",100,100,110);
-    trkana->Draw("demfit.mom.R()>>hist3","demfit.sid==0 && demlh.t0>=700", "HIST SAME"); //utilizing R() to make cuts at the entrance of the tracker and also at the time greater than 700 (ms?)
-    hist2->SetLineColor(kGreen);
-
-    TLegend* leg = new TLegend();
-   leg->AddEntry(hist, "e+ momentum  magnitude (MeV/c)", "l");
-   leg->AddEntry(hist2, "e+ momentum magnitude at entrance (MeV/c)", "l");
-   leg->AddEntry(hist3, "momentum magnitude at entrance (MeV/c) with t>700 (ns)","l");
-   leg->Draw();
-
-    c1->Update();
-    cout << "Saving canvas as ePlusTrkMomentum.pdf" << endl;
-    c1->SaveAs("ePlusTrkMomentum.pdf");
-
-    delete c1;
-    delete hist;
-    delete hist2;
-    delete hist3;
-  }
-
-  if (DO_ELECTRON_ORIGIN_MOMENTUM_COMP)
-  {
-    TChain* trkana = new TChain("TrkAna/trkana");
-
-    ifstream input_filelist("filelists/nts.mu2e.CeEndpointMix1BBSignal.Tutorial_2024_03.list");
-    if (input_filelist.is_open())
-    {
-      string filename;
-      while(getline(input_filelist,filename))
-      {
-        trkana->Add(filename.c_str());
-      }
-      input_filelist.close();
-    }
-
-    cout << "Number of entries: " << trkana->GetEntries() << endl;
-    TCanvas* c1 = new TCanvas("c1","c1");
-    c1->SetGridx(true);
-    c1->SetGridy(true);
-
-    TH1D* hist = new TH1D("hist","",100,100,110);
-    trkana->Draw("demfit.mom.R()>>hist","","HIST"); //utilizing the R() functions. plots magnitude of momentum vector and cut out the products until 700ns zone (eventually)
-
-    TH1D* hist2 = new TH1D("hist2","",100,100,110);
-    trkana->Draw("demmcsim.omom.R()>>hist2", "", "HIST SAME"); //again utilizing R() function. now plotting the thrown
-    hist2->SetLineColor(kRed);
-
-    //TH1D* hist3= new TH1D("hist3","",100,100,110);
-    //float comparison = demfit.mom.R()/demmcsim.omom.R();
-    //trkana->Draw(comparison,"", "HIST SAME"); //utilizing R() to plot the comparison histogram
-    //hist3->SetLineColor(kGreen);
-
-    TLegend* leg = new TLegend();
-   leg->AddEntry(hist, "e- momentum  magnitude (MeV/c)", "l");
-   leg->AddEntry(hist2, "e- momentum magnitude at entrance (MeV/c)", "l");
-   //leg->AddEntry(hist3, "e- momentum magnitude at entrance (MeV/c) with t>700 (ns)","l"); //selects specifically the data from the entrance and cuts electron momenta that appear before the selection region at 700ns
-   leg->Draw();
-
-    c1->Update();
-    cout << "Saving canvas as eTrkMomOComp.pdf" << endl;
-    c1->SaveAs("eTrkMomOComp.pdf");
-
-    delete c1;
-    delete hist;
-    delete hist2;
-    //delete hist3;
   }
 }
