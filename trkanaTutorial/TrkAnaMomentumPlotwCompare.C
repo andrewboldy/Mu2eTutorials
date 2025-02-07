ac708ba2671c91ed3c7f2078b0e73dfc4e7daa87
Andrew Boldy <aboldy@mu2egpvm01.fnal.gov>
2025-02-07 13:25:36 -0600

    Updates with set of end data

--- trkanaTutorial/TrkAnaMomentumPlotwCompare.C
+++ trkanaTutorial/TrkAnaMomentumPlotwCompare.C
@@ -32,6 +32,7 @@ void TrkAnaMomentumPlotwCompare()
       input_filelist.close();
     }
 
+    cout << "Number of entries: " << trkana->GetEntries() << endl;
     TCanvas* c1 = new TCanvas("c1","c1");
     c1->SetGridx(true);
     c1->SetGridy(true);
@@ -112,7 +113,7 @@ void TrkAnaMomentumPlotwCompare()
   if (DO_ELECTRON_ORIGIN_MOMENTUM_COMP==true)
   {
     TChain* trkana = new TChain("EventNtuple/ntuple"); //building a TChain from the directories TrkAnaNeg/trkana for a given file in the filelist
-
+    int fileNumber = 0;
     ifstream input_filelist("filelists/nts.mu2e.CeEndpointMix1BBTriggered.MDC2020an_v06_01_01_best_v1_3.list");
     if (input_filelist.is_open())
     {
@@ -120,22 +121,25 @@ void TrkAnaMomentumPlotwCompare()
       while(getline(input_filelist,filename))
       {
         trkana->Add(filename.c_str());
+        fileNumber=fileNumber+1;
       }
       input_filelist.close();
     }
-
-    cout << "Number of entries: " << trkana->GetEntries() << endl;
+    string fileNumberString = to_string(fileNumber);
+    cout << "Number of files analyzed: " << fileNumberString << endl;
+    //cout << "Number of total entries: " << trkana->GetEntries() << endl;
     TCanvas* c1 = new TCanvas("c1","c1");
     c1->SetGridx(true);
     c1->SetGridy(true);
 
     //branch 100 has trkmcsim --> need to determine where the start momentum and momentum are labeled. Says it takes info from SimInfo--> check code maybe
+    //Feb 6th, 2025--> Only producing a localization around the 104.9 MeV peak. Need to correct and understand what is happening here.
     TH1D* hist = new TH1D("hist","",100,100,110);
     trkana->Draw("trkmcsim.mom.R()>>hist","","HIST"); //utilizing the R() functions. plots magnitude of momentum vector for detected electrons //
 
-    //TH1D* hist2 = new TH1D("hist2","",100,100,110);
-    //trkana->Draw("trkmcsim.mom.R()>>hist2", "", "HIST SAME"); //again utilizing R() function. now plotting the thrown
-    //hist2->SetLineColor(kRed);
+    TH1D* hist2 = new TH1D("hist2","",100,100,110);
+    trkana->Draw("trkmcsim.endmom.R()>>hist2", "", "HIST SAME"); //again utilizing R() function. now plotting the thrown
+    hist2->SetLineColor(kRed);
 
     //TH1D* hist3= new TH1D("hist3","",100,100,110);
     //float comparison = demfit.mom.R()/demmcsim.omom.R();
@@ -143,8 +147,8 @@ void TrkAnaMomentumPlotwCompare()
     //hist3->SetLineColor(kGreen);
 
     TLegend* leg = new TLegend();
-   leg->AddEntry(hist, "measured e- momentum  magnitude (MeV/c)", "l");
-   //leg->AddEntry(hist2, "origin e- momentum magnitude (MeV/c)", "l");
+   leg->AddEntry(hist, "origin momentumof the SimParticle [MeV/c]", "l");
+   leg->AddEntry(hist2, "end momentumof the SimParticle [MeV/c]", "l");
    //leg->AddEntry(hist3, "measured e- momentum / origin e- momentum","l"); //selects specifically the data from the entrance and cuts electron momenta that appear before the selection region at 700ns
    leg->Draw();
 
@@ -154,7 +158,7 @@ void TrkAnaMomentumPlotwCompare()
 
     delete c1;
     delete hist;
-    //delete hist2;
+    delete hist2;
     //delete hist3;
   }
 }
