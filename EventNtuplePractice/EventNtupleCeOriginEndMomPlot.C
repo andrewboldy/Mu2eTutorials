e08d67a46694993d7130a937643e2c4544a25567
Andrew Boldy <aboldy@mu2egpvm02.fnal.gov>
2025-02-07 16:30:11 -0600

    included new 700 ns cut

--- EventNtuplePractice/EventNtupleCeOriginEndMomPlot.C
+++ EventNtuplePractice/EventNtupleCeOriginEndMomPlot.C
@@ -13,7 +13,8 @@
 #include <iostream>
 
 bool DO_ELECTRON_ORIGIN_MOMENTUM = false;
-bool DO_ELECTRON_END_MOMENTUM = true;
+bool DO_ELECTRON_END_MOMENTUM = false;
+bool DO_ELECTRON_END_MOMENTUM_AFTER_700 = true;
 
 using namespace std;
 
@@ -84,6 +85,40 @@ void EventNtupleCeOriginEndMomPlot()
     c1->Update();
     cout << "Saving canvas as endCeMomentum.pdf" << endl;
     c1->SaveAs("endCeMomentum.pdf");
+
+    delete c1;
+    delete hist;
+  }
+  if (DO_ELECTRON_END_MOMENTUM_AFTER_700==true)
+  {
+    TChain* ntuple = new TChain("EventNtuple/ntuple");
+
+    ifstream input_filelist("filelists/nts.mu2e.CeEndpointMix1BBTriggered.MDC2020an_v06_01_01_best_v1_3.list");
+    if (input_filelist.is_open())
+    {
+      string filename;
+      while(getline(input_filelist,filename))
+      {
+        ntuple->Add(filename.c_str());
+      }
+      input_filelist.close();
+    }
+
+    cout << "Number of entries: " << ntuple->GetEntries() << endl;
+    TCanvas* c1 = new TCanvas("c1","c1");
+    c1->SetGridx(true);
+    c1->SetGridy(true);
+
+    TH1D* hist = new TH1D("hist","",170,30,200);
+    ntuple->Draw("trkmcsim.endmom.R()>>hist","trkmcsim.time>=700","HIST");
+
+    TLegend* leg = new TLegend();
+    leg->AddEntry(hist, "end momentumof the SimParticle [MeV/c]", "l");
+    leg->Draw();
+
+    c1->Update();
+    cout << "Saving canvas as endCeMomentumAfter700.pdf" << endl;
+    c1->SaveAs("endCeMomentumAfter700.pdf");
 
     delete c1;
     delete hist;
